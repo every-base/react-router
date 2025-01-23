8462ed13bda1c7510a53d85514174014db38cfc7
tdd28 <tdd2810@gmail.com>
2025-01-23 18:27:32 +0700

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  publicDir: false,
   build: {
     lib: {
       entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter',
+      name: 'Router',
+      fileName: 'main',
     },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react/jsx-runtime': 'ReactJsxRuntime'
+        }
+      }
+    }
   },
 })
